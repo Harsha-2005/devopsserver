ee805bb55e9aed7f62812bbbbc961b0a64a3b50c
Harsha Pavan <mhemanarasimhaharshapavan@gmail.com>
2026-07-16 11:09:01 +0530

    Update server log message for clarity

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ app.get("/", (_req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port number ${port}`);
+  console.log(`Server is running on port number ${port}`);
 });
